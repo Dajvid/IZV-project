b7b1a48f7b4d2f78614cc051fc6e9726e1e0da40
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2021-12-11 20:58:45 +0100

    proj2 changes

--- analysis.py
+++ analysis.py
@@ -8,26 +8,7 @@ __email__ = "xsedla1d@stud.fit.vutbr.cz"
 from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
-import numpy as np
-import os
-
-# muzete pridat libovolnou zakladni knihovnu ci knihovnu predstavenou na prednaskach
-# dalsi knihovny pak na dotaz
-
-
-
-""" Ukol 1:
-načíst soubor nehod, který byl vytvořen z vašich dat. Neznámé integerové hodnoty byly mapovány na -1.
-
-Úkoly:
-- vytvořte sloupec date, který bude ve formátu data (berte v potaz pouze datum, tj sloupec p2a)
-- vhodné sloupce zmenšete pomocí kategorických datových typů. Měli byste se dostat po 0.5 GB. Neměňte však na kategorický typ region (špatně by se vám pracovalo s figure-level funkcemi)
-- implementujte funkci, která vypíše kompletní (hlubkou) velikost všech sloupců v DataFrame v paměti:
-orig_size=X MB
-new_size=X MB
-
-Poznámka: zobrazujte na 1 desetinné místo (.1f) a počítejte, že 1 MB = 1e6 B. 
-"""
+import pprint
 
 
 def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
@@ -39,7 +20,7 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
         Path to file with processed dataset in pickle format
     verbose : Bool
         When set to True, print the size of loaded dataset and size of
-        dataset after converting appropriate collumns to categoricla.
+        dataset after converting appropriate columns to categorical.
     Returns
     -------
     Parsed dataframe
@@ -47,14 +28,17 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
     df = pd.read_pickle(filename)
     df["date"] = df["p2a"].astype("datetime64")
     orig_size = df.memory_usage(deep=True).sum()
-    # p2b?, p14?, p47?, p53?, j?, n?, o?, r?
-    cat_cols = ["p36", "p37", "weekday(p2a)", "p6", "p7", "p8", "p9", "p10", "p11",
-                "p12", "p13a", "p13b", "p13c", "p15", "p16", "p17", "p18", "p19", "p20",
-                "p21", "p22", "p23", "p24", "p27", "p28", "p34", "p35", "p39", "p44",
-                "p45a", "p48a", "p49", "p50a", "p50b", "p51", "p52", "p55a", "p57",
-                "p58", "h", "i", "k", "l", "p", "q", "t", "p5a"]
+    # cat_cols = ["p36", "p37", "weekday(p2a)", "p6", "p7", "p8", "p9", "p10", "p11",
+    #             "p12", "p13a", "p13b", "p13c", "p15", "p16", "p17", "p18", "p19",
+    #             "p20",
+    #             "p21", "p22", "p23", "p24", "p27", "p28", "p34", "p35", "p39", "p44",
+    #             "p45a", "p48a", "p49", "p50a", "p50b", "p51", "p52", "p55a", "p57",
+    #             "p58", "h", "i", "k", "l", "p", "q", "t", "p5a"]
+    cat_cols = ["h", "i", "k", "l", "p", "q", "t", "n", "o"]  # n?
     df[cat_cols] = df[cat_cols].astype("category")
     new_size = df.memory_usage(deep=True).sum()
+    pp = pprint.PrettyPrinter(width=41, compact=True)
+    pp.pprint(dict(df.dtypes))
     if verbose:
         mibi = 1024 ** 2
         print(f"orig_size={orig_size / mibi :.1f} MB")
@@ -64,10 +48,21 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
 
 def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
                   show_figure: bool = False):
+    """Plot roadtype categorical plot.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe
+    fig_location : String
+        Path where to save the resulting figure.
+    show_figure : Bool
+        When set to True, figure is shown on the screen.
+    """
     if not fig_location and not show_figure:
         return
 
-    # TODO ordering?
+    # TODO sharex? sharey?
     df["p21"] = df["p21"].map({
         0: "Jiná komunikace",
         1: "Dvoupruhová komunikace",
@@ -80,8 +75,12 @@ def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
     df["Počet nehod"] = 1
     grouped = df.groupby(["region", "p21"]).agg({"Počet nehod": "sum"}).reset_index()
     displayed_regions = ["PHA", "JHM", "OLK", "ZLK"]
+
+    sns.set_theme()
+    sns.set(rc={'axes.facecolor': 'lightblue'})
     g = sns.catplot(data=grouped[grouped["region"].isin(displayed_regions)], x="region",
-                    y="Počet nehod", ci=None, col="p21", kind="bar",  col_wrap=3)
+                    y="Počet nehod", ci=None, col="p21", kind="bar",  col_wrap=3,
+                    sharex=True, sharey=True)
     g.set_axis_labels("Kraj", "Počet nehod").set_titles("{col_name}")
     if fig_location:
         plt.savefig(fig_location)
@@ -91,10 +90,21 @@ def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
 
 def plot_animals(df: pd.DataFrame, fig_location: str = None,
                  show_figure: bool = False):
+    """Plot animals categorical plot.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe
+    fig_location : String
+        Path where to save the resulting figure.
+    show_figure : Bool
+        When set to True, figure is shown on the screen.
+    """
+    # TODO sharex? sharey?
     if not fig_location and not show_figure:
         return
 
-    # TODO ordering?, axis_labels, hue_labels? != p10
     df["p10"] = df["p10"].map({
         1: "řidičem",
         2: "řidičem",
@@ -107,11 +117,14 @@ def plot_animals(df: pd.DataFrame, fig_location: str = None,
     })
     df["Počet nehod"] = 1
     df = df[df["date"] < "2021-01-01"]
-    grouped = df.groupby(["region", df['date'].dt.month, "p10"]).agg({"Počet nehod": "sum"}).reset_index()
+    df = df.groupby(["region", df['date'].dt.month, "p10"]).agg({"Počet nehod": "sum"})
     displayed_regions = ["PHA", "JHM", "OLK", "ZLK"]
-    g = sns.catplot(data=grouped[grouped["region"].isin(displayed_regions)], x="date",
-                    y="Počet nehod", kind="bar", ci=None, col="region", hue="p10",
-                    col_wrap=2)
+
+    sns.set_theme()
+    df = df.reset_index().rename(columns={"p10": "Zavinění"})
+    g = sns.catplot(data=df[df["region"].isin(displayed_regions)], x="date",
+                    y="Počet nehod", kind="bar", ci=None, col="region", hue="Zavinění",
+                    col_wrap=2, sharex=False, sharey=True)
     g.set_axis_labels("Měsíc", "Počet nehod").set_titles("Kraj: {col_name}")
     if fig_location:
         plt.savefig(fig_location)
@@ -119,9 +132,20 @@ def plot_animals(df: pd.DataFrame, fig_location: str = None,
         plt.show()
 
 
-# Ukol 4: Povětrnostní podmínky
 def plot_conditions(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
+    """Plot conditions relation plot.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe
+    fig_location : String
+        Path where to save the resulting figure.
+    show_figure : Bool
+        When set to True, figure is shown on the screen.
+    """
+    # TODO sharex? sharey?
     if not fig_location and not show_figure:
         return
 
@@ -136,25 +160,33 @@ def plot_conditions(df: pd.DataFrame, fig_location: str = None,
     })
     df = df[df["p18"] != 0]
     displayed_regions = ["PHA", "JHM", "OLK", "ZLK"]
-    displayed_regions = ["JHM", "MSK", "OLK", "ZLK"]
+    # displayed_regions = ["JHM", "MSK", "OLK", "ZLK"]
     df = df[df["region"].isin(displayed_regions)]
     df["Počet nehod"] = 1
     df = df[df["date"] < "2021-01-01"]
-    df = pd.pivot_table(df, columns="p18", index=["date", "region"], values="Počet nehod", aggfunc="sum")
-    #print(df.unstack(level=1).resample("M").sum().stack("region"))
-    df = df.unstack(level=1).resample("M").sum().stack("region").swaplevel(0,1)
-    # print(df)
+    df = df[df["date"] >= "2016-01-01"]
+    df = pd.pivot_table(df, columns="p18", index=["date", "region"],
+                        values="Počet nehod", aggfunc="sum")
+    df = df.unstack(level=1).resample("M").sum().stack("region").swaplevel(0, 1)
     df = df.melt(ignore_index=False).reset_index()
-    g = sns.relplot(data=df, kind="line", x="date", y="value", hue="p18", col="region", col_wrap=2) #sharex=false
-    g.set_axis_labels(None, "Počet nehod").set_titles("Kraj: {col_name}")
-    plt.show()
+    df = df.rename(columns={"p18": "Podmínky"})
+
+    sns.set_theme()
+    g = sns.relplot(data=df, kind="line", x="date", y="value",
+                    hue="Podmínky", col="region", col_wrap=2,
+                    facet_kws={'sharey': True, 'sharex': False})
+    g.set_axis_labels("", "Počet nehod").set_titles("Kraj: {col_name}")
+    g.set(xticks=["2016-01-01", "2017-01-01", "2018-01-01", "2019-01-01",
+                  "2020-01-01", "2021-01-01"])
+    g.set_xticklabels(["01/16", "01/17", "01/18", "01/19", "01/20", "01/21"])
+    if fig_location:
+        plt.savefig(fig_location)
+    if show_figure:
+        plt.show()
 
 
 if __name__ == "__main__":
-    # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
-    # skript nebude pri testovani pousten primo, ale budou volany konkreni ¨
-    # funkce.
-    df = get_dataframe("accidents.pkl.gz", verbose=True) # tento soubor si stahnete sami, při testování pro hodnocení bude existovat
-    # plot_roadtype(df, fig_location="01_roadtype.png", show_figure=True)
-    # plot_animals(df, "02_animals.png", True)
-    plot_conditions(df, "03_conditions.png", True)
+    dataframe = get_dataframe("accidents.pkl.gz", verbose=True)
+    plot_roadtype(dataframe, fig_location="01_roadtype.png", show_figure=True)
+    plot_animals(dataframe, "02_animals.png", True)
+    plot_conditions(dataframe, "03_conditions.png", True)
